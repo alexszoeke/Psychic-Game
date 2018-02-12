1b444c8d7476260f726286d72ca2ade06e0530ec
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-02-11 22:46:07 -0800

    Made tweaks to the game and made the page pretty

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,31 +5,41 @@ var losses = 0;
 var guessesLeft = 10;
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guessedLetters = [];
-console.log(alphabet.length);
+var computerGuess = "";
+var userGuess = "";
 
-//Function
+//Functions
+function computer() {
+computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+}
+
+function start() {
+
 document.onkeyup = function (event) {
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    userGuess = String.fromCharCode(event.keyCode).toLowerCase();
    console.log(userGuess);
-onkeyup = /a-z/;
 
 guessedLetters.push(userGuess);
 
-var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(computerGuess);
+
 
 //if user wins, their win count goes up and the game restarts without resetting stats
 if (userGuess === computerGuess) {
     wins++;
-    alert("You won! You're a legit psychic!")
+    alert("You won! You're a legit psychic! The correct letter was " + computerGuess);
     guessesLeft = 10;
     guessedLetters = [];
+    computer();
+    start();
     console.log(wins);
+    console.log("The letter I am thinking of is " + computerGuess);
 //when guesses reach 0, the game restarts without resetting stats
 } else if (userGuess) {
     guessesLeft--;
     console.log(guessesLeft);
     console.log(guessedLetters);
+    console.log("The letter I am thinking of is " + computerGuess);
 // } if (SOMETHING HERE IN A SECOND) {
 //     alert("You can only guess letters! Now we have to start over!");
 //     losses=0;
@@ -40,6 +50,7 @@ if (userGuess === computerGuess) {
     losses++;
     guessesLeft = 10;
     guessedLetters = [];
+    computer();
     alert("Well... You tried.");
 ;    console.log(losses);
 }
@@ -54,4 +65,10 @@ var html = "<p>Wins: " + wins + "</p>" +
 
 document.querySelector('#game').innerHTML = html;
 }
+}
 
+
+start();
+computer(computerGuess);
+console.log("The letter I am thinking of is " + computerGuess);
+console.log(userGuess);
