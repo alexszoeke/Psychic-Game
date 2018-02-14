4d448689bcbbfc369d3a0f194431f25eb46bf6ea
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-02-13 23:34:54 -0800

    fixed weird nonexistent yet existent hover attribute in my h1 tag? Also added comments in my javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,18 +9,24 @@ var computerGuess = "";
 var userGuess = "";
 
 //Functions
+
+//This function randomly generates the computers guess and places it into the computer variable above
 function computer() {
 computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 }
 
+
+//starts the games
 function start() {
 
+//users guess
 document.onkeyup = function (event) {
     userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-   console.log(userGuess);
-
-guessedLetters.push(userGuess);
+    console.log(userGuess);
+    
+    //Pushes the guessed letters into the array
+    guessedLetters.push(userGuess);
 
 
 
@@ -32,33 +38,24 @@ if (userGuess === computerGuess) {
     guessedLetters = [];
     computer();
     start();
-    console.log(wins);
-    console.log("The letter I am thinking of is " + computerGuess);
 //when guesses reach 0, the game restarts without resetting stats
 } else if (userGuess) {
     guessesLeft--;
-    console.log(guessesLeft);
-    console.log(guessedLetters);
-    console.log("The letter I am thinking of is " + computerGuess);
-// } if (SOMETHING HERE IN A SECOND) {
-//     alert("You can only guess letters! Now we have to start over!");
-//     losses=0;
-//     wins=0;
-//     guessedLetters=[];
-//when the user loses, their losses goes up and the game restarts without resetting stats
 } if (guessesLeft === 0) {
     losses++;
     guessesLeft = 10;
     guessedLetters = [];
     computer();
     alert("Well... You tried.");
-;    console.log(losses);
 }
 
+//see what's going on in the console
 console.log("Wins: " + wins);
 console.log("Losses: " + losses);
 console.log("Guesses Left: " + guessesLeft);
 
+
+//rewrite the stats in my html
 var html = "<p>Wins: " + wins + "</p>" + 
 "<p>Losses: " + losses + "</p>" + 
 "<p>Guesses Left: " + guessesLeft + "</p>" + "<p>Guessed Letters: " + guessedLetters + "</p>";
@@ -68,7 +65,8 @@ document.querySelector('#game').innerHTML = html;
 }
 
 
+//functions called
 start();
 computer(computerGuess);
+//hidden answer and the users guesses
 console.log("The letter I am thinking of is " + computerGuess);
-console.log(userGuess);
